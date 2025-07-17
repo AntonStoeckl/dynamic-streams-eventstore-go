8da695b5a76ac0a16db03cff2687f669b5b20d27
anton <anton@stoeckl.de>
2025-07-17 16:13:47 +0200

    Put EventFromJSON() into own file

--- events/event.go
+++ events/event.go
@@ -1,10 +1,5 @@
 package events
 
-import (
-	"errors"
-	"strings"
-)
-
 type EventTypeString = string
 type Events = []Event
 
@@ -14,52 +9,4 @@ type ReaderIDString = string
 type Event interface {
 	EventType() string
 	PayloadToJSON() ([]byte, error)
-}
-
-func EventFromJSON(eventType EventTypeString, payload []byte) (Event, error) {
-	switch eventType {
-	case BookCopyAddedToCirculationEventType:
-		event, unmarshallingErr := BookCopyAddedToCirculationFromJSON(payload)
-		if unmarshallingErr != nil {
-			return nil, errors.Join(errors.New("unmarshalling event from json failed"), unmarshallingErr)
-		}
-
-		return event, nil
-
-	case BookCopyRemovedFromCirculationEventType:
-		event, unmarshallingErr := BookCopyRemovedFromCirculationFromJSON(payload)
-		if unmarshallingErr != nil {
-			return nil, errors.Join(errors.New("unmarshalling event from json failed"), unmarshallingErr)
-		}
-
-		return event, nil
-
-	case BookCopyLentToReaderEventType:
-		event, unmarshallingErr := BookCopyLentToReaderFromJSON(payload)
-		if unmarshallingErr != nil {
-			return nil, errors.Join(errors.New("unmarshalling event from json failed"), unmarshallingErr)
-		}
-
-		return event, nil
-
-	case BookCopyReturnedByReaderEventType:
-		event, unmarshallingErr := BookCopyReturnedByReaderFromJSON(payload)
-		if unmarshallingErr != nil {
-			return nil, errors.Join(errors.New("unmarshalling event from json failed"), unmarshallingErr)
-		}
-
-		return event, nil
-
-	default:
-		if strings.Contains(eventType, SomethingHasHappenedEventTypePrefix) {
-			event, unmarshallingErr := SomethingHasHappenedFromJSON(payload)
-			if unmarshallingErr != nil {
-				return nil, errors.Join(errors.New("unmarshalling event from json failed"), unmarshallingErr)
-			}
-
-			return event, nil
-		}
-	}
-
-	return nil, errors.New("unknown event type")
 }
---
+++ events/event_from_json.go
@@ -0,0 +1,54 @@
+package events
+
+import (
+	"errors"
+	"strings"
+)
+
+func EventFromJSON(eventType EventTypeString, payload []byte) (Event, error) {
+	switch eventType {
+	case BookCopyAddedToCirculationEventType:
+		event, unmarshallingErr := BookCopyAddedToCirculationFromJSON(payload)
+		if unmarshallingErr != nil {
+			return nil, errors.Join(errors.New("unmarshalling event from json failed"), unmarshallingErr)
+		}
+
+		return event, nil
+
+	case BookCopyRemovedFromCirculationEventType:
+		event, unmarshallingErr := BookCopyRemovedFromCirculationFromJSON(payload)
+		if unmarshallingErr != nil {
+			return nil, errors.Join(errors.New("unmarshalling event from json failed"), unmarshallingErr)
+		}
+
+		return event, nil
+
+	case BookCopyLentToReaderEventType:
+		event, unmarshallingErr := BookCopyLentToReaderFromJSON(payload)
+		if unmarshallingErr != nil {
+			return nil, errors.Join(errors.New("unmarshalling event from json failed"), unmarshallingErr)
+		}
+
+		return event, nil
+
+	case BookCopyReturnedByReaderEventType:
+		event, unmarshallingErr := BookCopyReturnedByReaderFromJSON(payload)
+		if unmarshallingErr != nil {
+			return nil, errors.Join(errors.New("unmarshalling event from json failed"), unmarshallingErr)
+		}
+
+		return event, nil
+
+	default:
+		if strings.Contains(eventType, SomethingHasHappenedEventTypePrefix) {
+			event, unmarshallingErr := SomethingHasHappenedFromJSON(payload)
+			if unmarshallingErr != nil {
+				return nil, errors.Join(errors.New("unmarshalling event from json failed"), unmarshallingErr)
+			}
+
+			return event, nil
+		}
+	}
+
+	return nil, errors.New("unknown event type")
+}
