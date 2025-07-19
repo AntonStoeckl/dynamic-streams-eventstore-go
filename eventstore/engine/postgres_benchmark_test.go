ee15252ca54a4320ddf2f5461ef7e8b0b74975a3
anton <anton@stoeckl.de>
2025-07-19 11:16:57 +0200

    Improve benchmarks and add one more case (query+append)

--- eventstore/engine/postgres_benchmark_test.go
+++ eventstore/engine/postgres_benchmark_test.go
@@ -12,6 +12,8 @@ import (
 	"dynamic-streams-eventstore/config"
 	. "dynamic-streams-eventstore/eventstore/engine"
 	. "dynamic-streams-eventstore/test"
+	"dynamic-streams-eventstore/test/userland/core"
+	"dynamic-streams-eventstore/test/userland/shell"
 )
 
 func Benchmark_Append_With_Many_Events_InTheStore(b *testing.B) {
@@ -46,12 +48,19 @@ func Benchmark_Append_With_Many_Events_InTheStore(b *testing.B) {
 			assert.NoError(b, err, "error in running benchmark action")
 
 			b.StopTimer()
+
 			cmdTag, dbErr := connPool.Exec(
 				context.Background(),
 				fmt.Sprintf(`DELETE FROM events WHERE payload @> '{"BookID": "%s"}'`, bookID.String()),
 			)
 			assert.NoError(b, dbErr, "error in cleaning up benchmark artefacts")
 			assert.Equal(b, 1, int(cmdTag.RowsAffected()))
+
+			if i%100 == 0 {
+				_, dbErr = connPool.Exec(context.Background(), `vacuum analyze events`)
+				assert.NoError(b, dbErr, "error in cleaning up benchmark artefacts")
+			}
+
 			b.StartTimer()
 		}
 	})
@@ -87,6 +96,91 @@ func Benchmark_Query_With_Many_Events_InTheStore(b *testing.B) {
 
 		for i := 0; i < b.N; i++ {
 			QueryMaxSequenceNumberBeforeAppend(b, es, filter)
+		}
+	})
+}
+
+func Benchmark_TypicalWorkload_With_Many_Events_InTheStore(b *testing.B) {
+	// setup
+	factor := 1000 // multiplied by 1000 -> total num of fixture events
+	connPool, err := pgxpool.NewWithConfig(context.Background(), config.DBBenchmarkConfig())
+	defer connPool.Close()
+	assert.NoError(b, err, "error connecting to DB pool in test setup")
+
+	es := NewPostgresEventStore(connPool)
+
+	// arrange
+	appendFixtureEvents(b, connPool, es, factor)
+
+	bookID := GivenUniqueID(b)
+	readerID := GivenUniqueID(b)
+	filter := FilterAllEventTypesForOneBookOrReader(bookID, readerID)
+
+	// act
+	b.Run("append", func(b *testing.B) {
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			b.StopTimer()
+
+			GivenBookCopyAddedToCirculationWasAppended(b, es, bookID)
+			GivenBookCopyLentToReaderWasAppended(b, es, bookID, readerID)
+
+			b.StartTimer()
+
+			storableEvents, maxSequenceNumberBeforeAppend, queryErr := es.Query(filter)
+			assert.NoError(b, queryErr, "error in running benchmark query")
+			domainEvents, mappingErr := shell.DomainEventsFrom(storableEvents)
+			assert.NoError(b, mappingErr, "error in mapping events for benchmark")
+
+			// business logic for this feature/use-case
+			bookExists := false
+			bookCurrentlyLentOutByThisReader := false
+			for _, domainEvent := range domainEvents {
+				switch domainEvent.EventType() {
+				case core.BookCopyAddedToCirculationEventType:
+					bookExists = true
+
+				case core.BookCopyRemovedFromCirculationEventType:
+					bookExists = false
+
+				case core.BookCopyLentToReaderEventType:
+					actualEvent := domainEvent.(core.BookCopyLentToReader)
+					bookCurrentlyLentOutByThisReader = actualEvent.ReaderID == readerID.String()
+
+				case core.BookCopyReturnedByReaderEventType:
+					actualEvent := domainEvent.(core.BookCopyReturnedByReader)
+					bookCurrentlyLentOutByThisReader = actualEvent.ReaderID == readerID.String()
+				}
+			}
+
+			assert.True(b, bookExists, "book should exist")
+			assert.True(b, bookCurrentlyLentOutByThisReader, "book should be lent out by this reader")
+
+			b.StartTimer()
+
+			err = es.Append(
+				ToStorable(b, BuildBookCopyReturnedFromReader(bookID, readerID)),
+				filter,
+				maxSequenceNumberBeforeAppend,
+			)
+			assert.NoError(b, err, "error in running benchmark action")
+
+			b.StopTimer()
+
+			cmdTag, dbErr := connPool.Exec(
+				context.Background(),
+				fmt.Sprintf(`DELETE FROM events WHERE payload @> '{"BookID": "%s"}'`, bookID.String()),
+			)
+			assert.NoError(b, dbErr, "error in cleaning up benchmark artefacts")
+			assert.Equal(b, 3, int(cmdTag.RowsAffected()))
+
+			if i%100 == 0 {
+				_, dbErr = connPool.Exec(context.Background(), `vacuum analyze events`)
+				assert.NoError(b, dbErr, "error in cleaning up benchmark artefacts")
+			}
+
+			b.StartTimer()
 		}
 	})
 }
