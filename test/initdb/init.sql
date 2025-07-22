fab4b450a09b5a72f6256e3ac5ee407f7659f9a1
anton <anton@stoeckl.de>
2025-07-22 08:29:15 +0200

    Create occurredAt at the application side instead of inside the DB

--- test/initdb/init.sql
+++ test/initdb/init.sql
@@ -1,6 +1,6 @@
 CREATE TABLE if not exists events (
     sequence_number BIGSERIAL PRIMARY KEY,
-    occurred_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    occurred_at TIMESTAMPTZ NOT NULL,
     event_type TEXT NOT NULL,
     payload JSONB NOT NULL
 );
@@ -9,6 +9,8 @@ CREATE TABLE if not exists events (
 CREATE INDEX if not exists idx_events_event_type ON events(event_type);
 CREATE INDEX if not exists idx_events_occurred_at ON events(occurred_at);
 CREATE INDEX if not exists idx_events_payload_gin ON events USING gin(payload);
+
+-- I experimented with compound indexes, but it does not seem to make queries faster ...
 -- CREATE EXTENSION if not exists btree_gin;
 -- CREATE INDEX if not exists idx_events_event_type_payload_gin ON events USING gin(event_type, payload);
 -- CREATE INDEX if not exists idx_events_event_type_payload_occurred_at_gin ON events USING gin(event_type, payload, occurred_at);
