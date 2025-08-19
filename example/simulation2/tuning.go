430141efde8dcb2e93351428b98680e76ad3fa5b
anton <anton@stoeckl.de>
2025-08-19 12:06:13 +0200

    Start with 50 initial readers

--- example/simulation2/tuning.go
+++ example/simulation2/tuning.go
@@ -19,7 +19,7 @@ const (
 	// ACTOR POOL CONFIGURATION ...
 
 	// InitialActiveReaders defines the conservative starting point for active readers.
-	InitialActiveReaders = 150
+	InitialActiveReaders = 50
 	// MinActiveReaders defines the minimum scale limit.
 	MinActiveReaders = 30
 	// MaxActiveReaders defines the upper safety limit.
