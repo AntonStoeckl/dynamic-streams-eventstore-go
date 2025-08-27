31b5e430813ac2366b0640b4552ff1d0a6a584d4
anton <anton@stoeckl.de>
2025-08-27 13:57:58 +0200

    Remove impossible else case in a projection

--- example/features/command/lendbookcopytoreader/decide.go
+++ example/features/command/lendbookcopytoreader/decide.go
@@ -116,8 +116,6 @@ func project(history core.DomainEvents, bookID string, readerID string) state { 
 			if e.BookID == bookID {
 				if e.ReaderID == readerID {
 					s.bookIsLentToThisReader = false
-				} else {
-					s.bookIsLentToAnotherReader = false
 				}
 			}
 
